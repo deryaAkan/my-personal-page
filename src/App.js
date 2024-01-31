569e95f6f82333ba66ae38dd4c7fd1abf00e056e
Derya Akan <d.akan369@gmail.com>
2024-01-31 16:25:23 +0300

    profile css giris

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React from 'react';
 import './App.css';
 import Projects from './components/Projects';
 import Header from './components/Header';
-import Profile from './components/Profile';
+import Skills from './components/Skills';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Profile></Profile>
+      <Skills></Skills>
       <Projects></Projects>
     </div>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,18 @@
 import "../App.css";
+import profileImg from '../assests/Alternative-1/foto.png';
+
 
 export default function Header() {
   return (
     <header className="Header">
-      <div>
-        <div className="hi almilaaa"></div>
-        <img src="" className="PROFILEEE" />
+      <div className="w-3/5 flex">
+        <div className="flex-col">
+          <h1 className="text-[#CBF281] text-[54px] font-bold">I am a Frontend Developer...</h1>
+          <p className="text-[24px] font-normal">...who likes to craft solid and scalable frontend products with great user experiences.</p>
+          <button className="rounded bg-[#CBF281] text-[#4731D3]">Github</button>
+          <button className="rounded bg-[#CBF281] text-[#4731D3]">LinkedIn</button>
+        </div>
+        <img src={profileImg} className="w-1/3" />
       </div>
     </header>
   );
